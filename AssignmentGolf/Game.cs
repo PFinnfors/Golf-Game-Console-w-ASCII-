d6fd271565d22bd6e3576479532703f3f92396b0
PFinnfors <pier@live.se>
2017-02-14 11:54:25 +0100

    Add project files.

---
+++ AssignmentGolf/Game.cs
@@ -0,0 +1,175 @@
+﻿/* NOTE: I designed the full game without the use of exception handling by building the limitations into the code.
+ * Then when I realized I had to use them, I started modifying the code to fit it in and this turned out to be
+ * more difficult than I thought, and has made the code messier. This assignment would've been done faster and
+ * better if I had read the instructions more carefully and designed it accordingly from the beginning. */
+
+using System;
+using System.Threading;
+
+namespace AssignmentGolf
+{
+    class Game
+    {
+        static void Main(string[] args)
+        {
+            //PREPARATIONS
+            int[,] strokeValues = new int[8, 2]; //0,0 = Angle; 1,0 = Velocity/stroke distance;
+                                                 //2,0 = Start distance; 3,0 = Hole distance
+                                                 //Later used to store for-loop value to give a turn count
+            int turns = 0;
+
+            //Creates a new class instance to put methods into, to give code room
+            Stroke strokeInputClass = new Stroke();
+
+            //Main game loop, broken or preserved at the end with user input
+            char exitKey = 'Y';
+            //does while exitKey is Y
+            do
+            {
+                //Used later to check if user has given an exitKey response to break out of exit question
+                bool exitBool = false;
+
+                //Generates a pseudo-random golf course between 1.2km-2.4km
+                Random rand = new Random();
+                strokeValues[3, 0] = rand.Next(1200, 2400);
+
+                //GAME START
+                Console.WriteLine("     '\\                   .  .      Art by Joan Stark |>18>>\n"
+                                + "       \\              .         ' .                   |\n"
+                                + "      O>>         .                 'o                |\n"
+                                + "       \\       .       WELCOME                        |\n"
+                                + "       /\\    .         TO THE                         |\n"
+                                + "      / /  .'         GOLF GAME           P.FINNFORS  |\n"
+                                + "jgs^^^^^^^`^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n");
+
+                //TURN STARTS: Loops this gameplay and keeps stroke count through i
+                for (int i = 1; i < 20; i++)
+                {
+                    //Since turn exception comes later, this if keeps following code from executing
+                    //after the last turn while strokes left are zero
+                    if (i < 9)
+                    {
+                        Console.WriteLine("IT'S YOUR TURN!");
+
+                        //Shows stroke count only after there have been strokes
+                        if (i > 1)
+                            Console.Write($" You've hit the ball [{i - 1}] time(s). ");
+                        Console.Write($"You have a total of [{9 - i}] stroke(s) left.\n");
+
+                        //getStroke called to take care of the gameplay
+                        strokeValues = strokeInputClass.getStroke(strokeValues);
+                        //processStroke called to take care of the turn results
+                        strokeValues = strokeInputClass.processStroke(strokeValues);
+
+                        Thread.Sleep(1000);
+                    }
+
+                    //Stores turn count from the ongoing for-loop
+                    turns = i;
+
+                    //CHECKS CONDITIONS FOR WIN OR LOSS
+                    //The ball hits the hole when hole distance is zero -- wins
+                    if (strokeValues[3, 0] == 0)
+                    {
+                        Console.WriteLine("-----------------------------------------------------------o\n\n"
+                            + "[The ball falls into the hole...] YOU WON!\n"
+                            + $"\nIt took you [{i}] stroke(s).\n"
+                            + " __          _______ _   _ _   _ ______ _____  \n"
+                            + " \\ \\        / /_   _| \\ | | \\ | |  ____|  __ \\ \n"
+                            + "  \\ \\  /\\  / /  | | |  \\| |  \\| | |__  | |__) |\n"
+                            + "   \\ \\/  \\/ /   | | | . ` | . ` |  __| |  _  / \n"
+                            + "    \\  /\\  /   _| |_| |\\  | |\\  | |____| | \\ \\ \n"
+                            + "     \\/  \\/   |_____|_| \\_|_| \\_|______|_|  \\_\\\n"
+                            + "-----------------------------------------------------------o\n\n");
+                        i = 20; //Breaks for loop
+                    }
+
+                    //RECORDS STROKES AND USES INDEX OUT OF RANGE EXCEPTION TO SEND GAME OVER MESSAGE
+                    try
+                    {
+                        if (i < 10)
+                            strokeValues[i - 1, 1] = strokeValues[1, 0];
+                    }
+                    catch
+                    {
+                        Console.WriteLine("----------------------------------------------------------------o\n\n"
+                        + "OH NO! You used up all your strokes...\n"
+                        + "  _____          __  __ ______    ______      ________ _____  \n"
+                        + " / ____|   /\\   |  \\/  |  ____|  / __ \\ \\    / /  ____|  __ \\ \n"
+                        + "| |  __   /  \\  | \\  / | |__    | |  | \\ \\  / /| |__  | |__) |\n"
+                        + "| | |_ | / /\\ \\ | |\\/| |  __|   | |  | |\\ \\/ / |  __| |  _  / \n"
+                        + "| |__| |/ ____ \\| |  | | |____  | |__| | \\  /  | |____| | \\ \\ \n"
+                        + " \\_____/_/    \\_\\_|  |_|______|  \\____/   \\/   |______|_|  \\_\\\n"
+                        + "----------------------------------------------------------------o\n\n");
+                        i = 20; //Breaks for loop
+                    }
+
+                    //CAUSES A DIVISION BY ZERO EXCEPTION IF VALUE 2800 IS REACHED (OUTSIDE COURSE)
+                    // (& i != 20) ensures exceptions don't follow each other
+                    if (strokeValues[3, 0] >= 2800 & i != 20)
+                    {
+                        try
+                        {
+                            //Division by a zero value
+                            strokeValues[2, 0] = 2800 / strokeValues[6, 0];
+                        }
+                        catch
+                        {
+                            Console.WriteLine("-----------------------------------------------------------o\n\n"
+                           + "WHOOPS! You shot the ball out of the course. Poor lady on the sidewalk!\nGAME OVER!\n\n"
+                           + "-----------------------------------------------------------o\n"
+                           + " ______     _    ___   __  _____         _____ \n"
+                           + "|  ____/\\  | |  | \\ \\ / / |  __ \\ /\\    / ____|\n"
+                           + "| |__ /  \\ | |  | |\\ V /  | |__) /  \\  | (___  \n"
+                           + "|  __/ /\\ \\| |  | | > <   |  ___/ /\\ \\  \\___ \\ \n"
+                           + "| | / ____ \\ |__| |/ . \\  | |  / ____ \\ ____) |\n"
+                           + "|_|/_/    \\_\\____//_/ \\_\\ |_| /_/    \\_\\_____/ \n\n");
+
+                            i = 20;
+                        }
+                    }
+                }
+                //THE GAME IS OVER
+                //Loop to write out array values *turns* amount of times
+                for (int t = 1; t <= turns; t++)
+                {
+                    try { Console.WriteLine($"Turn {t}: [{strokeValues[t - 1, 1]}m]"); }
+                    catch { /* Doesn't attempt to write out overbound value */ }
+
+                }
+                //Asks question until exitBool is true
+                while (exitBool == false)
+                {
+                    Console.WriteLine("\n\nDo you want to play again (Y/N):");
+                    exitKey = Console.ReadKey().KeyChar;
+                    exitKey = char.ToUpper(exitKey);
+                    Console.WriteLine("");
+
+                    if (exitKey == 'Y')
+                    {
+                        Console.WriteLine("\nNice, go practice those swings!");
+                        Thread.Sleep(2000);
+                        Console.WriteLine("\n * NEW GAME * \n\n");
+                        //Question satisfied
+                        exitBool = true;
+                    }
+                    else if (exitKey == 'N')
+                    {
+                        Console.WriteLine("\nOK. Goodbye!");
+                        Thread.Sleep(1000);
+                        //Question satisfied
+                        exitBool = true;
+                    }
+                    else
+                    {
+                        //Given error message then sent back to question via while-loop
+                        if (exitKey != 'Y' && exitKey != 'N')
+                            Console.WriteLine("----------------------------o\n"
+                                + "Answer must be Yes or No!");
+                    }
+                }
+
+            } while (exitKey == 'Y');
+        }
+    }
+}
---
+++ AssignmentGolf/Stroke.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace AssignmentGolf
+{
+    public class Stroke
+    {
+        //METHOD GETS MAIN GAMEPLAY
+        public int[,] getStroke(int[,] strokeResult)
+        {
+            
+
+            //Player is asked to give a stroke angle, code calls getStrokeInput() to receive the value
+            Console.WriteLine($"\nYou're swinging [{strokeResult[3,0]}m] from the hole on a random course. "
+            + "\n\nWhat angle do you want to strike the ball with?"
+                + "\nAngle (1-179):");
+
+                strokeResult[0, 0] = getStrokeInput();
+            
+            while (strokeResult[0,0] > 179 || strokeResult[0,0] < 1)
+            {
+                if (strokeResult[0,0] < 0 || strokeResult[0,0] > 180)
+                    Console.WriteLine("\n[You spin the club around...] Are you going to strike or what?\nTry again (1-179):");
+                else
+                    Console.WriteLine("\n[You smash the ball into the grass...] Don't be mad, it's just a game!\nTry again (1-179):");
+
+                strokeResult[0,0] = getStrokeInput();
+            }
+
+            //Player is asked to give a stroke velocity, code calls getStrokeInput() again
+            Console.WriteLine($"\nOkay, you're swinging at a {strokeResult[0,0]} degree angle. How fast (m/s)?"
+                + "\nVelocity (1-94):");
+            //Gets input to be calculated into distance
+            strokeResult[1,0] = getStrokeInput();
+            //Sets velocity constraints and error message
+            while (strokeResult[1,0] < 1 || strokeResult[1,0] > 94)
+            {
+                Console.WriteLine("You can't hit slower than 1 or faster than 94 m/s. This isn't a game! ¯\\_(ツ)_/¯ "
+                    + "\nTry again (1-94):");
+                strokeResult[1,0] = getStrokeInput();
+            }
+
+            return strokeResult;
+        }
+
+        //METHOD CALLED DURING GAMEPLAY TO TAKE CARE OF INPUT WITH PARSE CHECK
+        public int getStrokeInput()
+        {
+            string inputString;
+            int inputValue = 0;
+            bool parseCheck = false, loopCheck = false;
+
+            //Until the parse succeeds, it keeps asking for the input
+            while (!parseCheck)
+            {
+                //Shows error message once there has been a loop and loopCheck is set to true
+                if (loopCheck) { Console.WriteLine("Huh, what's that? Try again:"); }
+
+                inputString = Console.ReadLine();
+                parseCheck = int.TryParse(inputString, NumberStyles.AllowThousands, CultureInfo.InvariantCulture, out inputValue);
+                loopCheck = true;
+            }
+            //Resets checks for next use
+            parseCheck = false;
+            loopCheck = false;
+            return inputValue;
+        }
+
+        //METHOD: PROCESSES THE OUTCOME / STROKE VALUES
+        public int[,] processStroke(int[,] strokeResults)
+        {
+            //Calculates new angle in radians
+            double angleResult = (Math.PI / 180) * strokeResults[0,0];
+
+            //Calculates the resulting distance
+            double distanceResult = System.Convert.ToDouble(strokeResults[1,0]);
+            distanceResult = Math.Pow(distanceResult, 2) //Velocity^2
+                / 9.8 * Math.Sin(2 * angleResult); //Divided by gravity constant, multiplied by radians arc
+            distanceResult = Math.Round(distanceResult, 0, MidpointRounding.ToEven);
+            
+            //Updates new distance to hole
+            strokeResults[3,0] = (strokeResults[3,0] - (Convert.ToInt32(distanceResult)) );
+            strokeResults[3,0] = Math.Abs(strokeResults[3,0]);
+
+            //Gives result to player
+            Thread.Sleep(350);
+            Console.WriteLine("\n*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n"
+                             + $"The ball flew {distanceResult}m...\n"
+                             + "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*\n");
+            
+            //Converting and sending back to integer array
+            strokeResults[1,0] = Convert.ToInt32(distanceResult);
+            strokeResults[2,0] = Convert.ToInt32(strokeResults[2,0] + strokeResults[1,0]);
+
+            return strokeResults;
+        }
+
+
+    }
+        
+}
